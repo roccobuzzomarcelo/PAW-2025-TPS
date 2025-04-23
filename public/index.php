4f742a6d896ddfba6c77cf3cef6424542c7bd15f
Eliana <elianacardona12@gmail.com>
2025-04-23 20:46:30 -0300

    incorporación de menú con php

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+$menu = [
+    [
+        "href" => "/catalogo/catalogo.html",
+        "name" => "Catálogo"
+    ],
+    [
+        "href" => "/catalogo/mas-vendidos.html",
+        "name" => "Más vendidos"
+    ],
+    [
+        "href" => "/catalogo/novedades.html",
+        "name" => "Novedades"
+    ],
+    [
+        "href" => "/catalogo/recomendados.html",
+        "name" => "Recomendados"
+    ],
+    [
+        "href" => "/catalogo/promociones.html",
+        "name" => "Promociones"
+    ],
+    [
+        "href" => "preguntas/como-comprar.html",
+        "name" => "Como comprar"
+    ],
+    [
+        "href" => "mi-cuenta/mi-cuenta.html",
+        "name" => "Mi cuenta"
+    ]
+];
+    require 'index.view.php'; 
+?>
