a20e48ebb8995721695c1da629626db9b710722c
Bautista Pereyra Buch <bautistapereyrabuch@gmail.com>
2025-05-02 20:16:41 -0300

    composer + directorio src

--- public/index.php
+++ public/index.php
@@ -30,8 +30,24 @@ $menu = [
         "name" => "Mi cuenta"
     ]
 ];
-    require './includes/header.php';
-    require 'index.view.php';
-    require './includes/footer.php';
 
-?>
+require '../src/includes/header.php';
+require '../src/index.view.php';
+require '../src/includes/footer.php';
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if($path == "/"){
+    require '../src/index.view.php';
+}else if($path == 'about'){
+    require '../src/about.view.php';
+}elseif($path == 'catalogo'){
+    require '../src/catalog.view.php';
+}elseif($path == 'preguntas'){
+    require '../src/faq.view.php';
+}elseif($path == 'mi-cuenta'){
+    require '../src/account.view.php';
+}else{
+    http_response_code(404);
+    require '../src/404.view.php';
+}
