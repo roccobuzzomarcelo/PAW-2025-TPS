d5d3f8a13022ea0b36fecc2c154bca12b1a93b91
Bautista Pereyra Buch <bautistapereyrabuch@gmail.com>
2025-05-02 20:23:59 -0300

    Incluir Whoops

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 $menu = [
     [
         "href" => "/catalogo/catalogo.html",
