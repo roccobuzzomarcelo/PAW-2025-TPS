f8cc4f64a472efd66f5c894b7ae1ba6f156abdfa
Bautista Pereyra Buch <bautistapereyrabuch@gmail.com>
2025-05-05 01:50:20 -0300

    Separar el head

--- src/App/Controlador/ControladorPagina.php
+++ src/App/Controlador/ControladorPagina.php
@@ -73,6 +73,7 @@ class ControladorPagina{
     
 
     public function index(){
+        $titulo = "PawPrints - Inicio";
         $novedades = $this->obtenerLibros(null, [5, 7]); // IDs de los libros nuevos
         $masVendidos = $this->obtenerLibros(null, [1, 3, 6]); // IDs de los libros más vendidos
         $recomendados = $this->obtenerLibros(null, [1, 2, 3, 4, 5]); // IDs de los libros recomendados
@@ -80,52 +81,73 @@ class ControladorPagina{
     }
 
     public function catalogo(){
+        $titulo = "PawPrints - Catálogo";
+        $htmlClass = "catalogo-pages";
         $consulta = $_GET['consulta'] ?? null;
         $libros = $this->obtenerLibros($consulta);
         require $this->viewsDir .'catalog.view.php';
     }
 
     public function masVendidos(){
+        $titulo = "PawPrints - Más vendidos";
+        $htmlClass = "catalogo-pages";
         $libros = $this->obtenerLibros(null, [1, 3, 6]); // IDs de los libros más vendidos
         require $this->viewsDir . 'mas-vendidos.view.php';
     }
 
     public function novedades(){
+        $titulo = "PawPrints - Novedades";
+        $htmlClass = "catalogo-pages";
         $libros = $this->obtenerLibros(null, [5, 7]); // IDs de los libros nuevos
         require $this->viewsDir . 'novedades.view.php';
     }
 
     public function recomendados(){
+        $titulo = "PawPrints - Recomendados";
+        $htmlClass = "catalogo-pages";
         $libros = $this->obtenerLibros(null, [1, 2, 3, 4, 5]); // IDs de los libros recomendados
         require $this->viewsDir . 'recomendados.view.php';
     }
 
     public function promociones(){
+        $titulo = "PawPrints - Promociones";
+        $htmlClass = "catalogo-pages";
         $libros = $this->obtenerLibros(null, [2, 3, 4]); // IDs de los libros en promoción
         require $this->viewsDir . 'promociones.view.php';
     }
 
     public function comoComprar(){
+        $titulo = "PawPrints - Cómo comprar";
+        $htmlClass = "preguntas-pages";
         require $this->viewsDir . 'como-comprar.view.php';
     }
 
     public function miCuenta(){
+        $titulo = 'PawPrints - Mi cuenta';
+        $htmlClass = "mi-cuenta-pages";
         require $this->viewsDir . 'mi-cuenta.view.php';
     }
 
     public function quienesSomos(){
+        $titulo = 'PawPrints - Quiénes somos';
+        $htmlClass = "preguntas-pages";
         require $this->viewsDir . 'quienes-somos.view.php';
     }
 
     public function locales(){
+        $titulo = 'PawPrints - Locales';
+        $htmlClass = "preguntas-pages";
         require $this->viewsDir . 'locales.view.php';
     }
 
     public function carrito(){
+        $titulo = 'PawPrints - Carrito de compras';
+        $htmlClass = "carrito-pages";
         require $this->viewsDir . 'carrito.view.php';
     }
 
-    public function detalleLibro(){
+    public function detalleLibro(){;
+        $htmlClass = "libro-pages";
         $id = $_GET['id'] ?? null;
         $libros = $this->obtenerLibros(null, [$id]);
         if (empty($libros)) {
@@ -135,6 +157,7 @@ class ControladorPagina{
         }
         $libro = $libros[0];
         $mismoAutorLibros = $this->obtenerLibros($libro['autor']);
+        $titulo = htmlspecialchars($libro['titulo']);
         require $this->viewsDir . 'detalle-libro.view.php';
     }
 
--- src/App/views/detalle-libro.view.php
+++ src/App/views/detalle-libro.view.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html class="not-found" lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/styles.css">
-    <link rel="stylesheet" href="../styles/print.css" media="print">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <title><?= htmlspecialchars($libro['titulo']) ?></title>
-</head>
+<?php include 'parts/head.php'?>
 <body>
 <?php include "parts/header.php"; ?>
     <main>
