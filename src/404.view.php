d5c41e14fb1f610bb88900f84b363f87216bc6df
Bautista Pereyra Buch <bautistapereyrabuch@gmail.com>
2025-05-04 21:10:29 -0300

    Cambio en not-found page

--- src/404.view.php
+++ src/404.view.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
-<html lang="es">
+<html class="not-found" lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <title>Error 404 - Página no encontrada</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="stylesheet" href="../styles/print.css" media="print">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <title>Pagina no encontrada</title>
 </head>
 <body>
-    <h1>¡Página no encontrada!</h1>
-    <p>Lo sentimos, la página que buscas no existe.</p>
+<?php include __DIR__ . '/includes/header.php'; ?>
+    <main>
+        <!-- BREADCRUMB -->
+
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item" aria-current="page">No encontrada</li>
+        </ul>
+        <h1 class="error-titulo">¡Página no encontrada!</h1>
+        <p class="error-descripcion">Lo sentimos, la página que buscas no existe o ha sido movida.</p>
+        <a href="/" class="boton-volver">Volver al inicio</a>
+    </main>
 </body>
 </html>
