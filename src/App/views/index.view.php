b6e341000a0d85cf1b367fcda2989c2ce5fdb78f
rocco <roccobuzzomarcelo@gmail.com>
2025-05-21 00:43:06 -0300

    modificación en el style.css y el pawmenu.css

--- src/App/views/index.view.php
+++ src/App/views/index.view.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html class="<?= htmlspecialchars($htmlClass ?? '') ?>" lang="es">
-<?php include 'parts/head.php'?>
-<body>        
+<?php include 'parts/head.php' ?>
+
+<body>
 
     <?php include 'parts/header.php'; ?>
-    
+
     <main>
         <!-- BREADCRUMB -->
 
@@ -12,7 +13,7 @@
             <li class="breadcrumb-item" aria-current="page">Home</li>
         </ul>
         
-
+        <!-- CAROUSEL -->
         <div class="carousel-container">
             <img src="/images/carousel/imagen1.jpg" />
             <img src="/images/carousel/imagen2.jpg" />
@@ -23,25 +24,28 @@
         <!-- NOVEDADES -->
         <h2 class="subtitulo">Novedades</h2>
         <section class="cont-libros">
-            <?php $libros = $novedades; include 'parts/mostrarLibros.php'; ?>
+            <?php $libros = $novedades;
+            include 'parts/mostrarLibros.php'; ?>
         </section>
 
         <!-- MÁS VENDIDOS -->
         <h2 class="subtitulo">Más Vendidos</h2>
 
         <section class="cont-libros">
-            <?php $libros = $masVendidos; include 'parts/mostrarLibros.php'; ?>
+            <?php $libros = $masVendidos;
+            include 'parts/mostrarLibros.php'; ?>
         </section>
 
-        <!-- Recomendados -->
+        <!-- RECOMENDADOS -->
         <h2 class="subtitulo">Recomendados</h2>
 
         <section class="cont-libros">
-            <?php $libros = $recomendados; include 'parts/mostrarLibros.php'; ?>
+            <?php $libros = $recomendados;
+            include 'parts/mostrarLibros.php'; ?>
         </section>
     </main>
 
-   <?php include 'parts/footer.php'; ?> 
+    <?php include 'parts/footer.php'; ?>
 </body>
 
 </html>
